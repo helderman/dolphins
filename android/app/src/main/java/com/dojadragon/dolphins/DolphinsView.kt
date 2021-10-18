c21ecdbd42849d348f7f9414ca1818784f6afd92
Ruud Helderman <r.helderman@hccnet.nl>
2021-10-19 01:09:43 +0200

    DolphinsView; buttons

---
+++ android/app/src/main/java/com/dojadragon/dolphins/DolphinsView.kt
@@ -0,0 +1,44 @@
+package com.dojadragon.dolphins
+
+import android.content.Context
+import android.os.SystemClock
+import android.util.AttributeSet
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class DolphinsView(context: Context, attrs: AttributeSet) : SurfaceView(context, attrs), SurfaceHolder.Callback {
+    private val drawer = DolphinsDrawer(context, SystemClock::elapsedRealtime)
+
+    init {
+        holder.addCallback(this)
+        isFocusable = true
+    }
+
+    private val runnable = Runnable { step() }
+
+    private var willDraw = false
+
+    override fun surfaceCreated(p0: SurfaceHolder) {
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        drawer.setDimensions(width, height)
+        willDraw = true
+        step()
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        willDraw = false
+        step()
+    }
+
+    private fun step() {
+        if (willDraw) {
+            drawer.drawFrame(holder)
+        }
+        handler.removeCallbacks(runnable)
+        if (willDraw) {
+            handler.postDelayed(runnable, 20)
+        }
+    }
+}
--- android/app/src/main/java/com/dojadragon/dolphins/MainActivity.kt
+++ android/app/src/main/java/com/dojadragon/dolphins/MainActivity.kt
@@ -1,24 +1,52 @@
 package com.dojadragon.dolphins
 
-import android.content.Context
-import android.graphics.Canvas
+import android.app.WallpaperManager
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    //private val drawer = DolphinsDrawer(this, SystemClock::elapsedRealtime)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        //setContentView(SampleView(this, DolphinsDrawer(this)))
-    }
 
-    /*
-    inner class SampleView(context: Context?, private val drawer: DolphinsDrawer) : View(context) {
-        override fun onDraw(canvas: Canvas?) {
-            //super.onDraw(canvas)
-            invalidate()
+        /*
+        startActivity(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) intentActivate() else intentList())
+        finish()
+
+         */
+
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btnActivate).setOnClickListener {
+            startActivity(if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) intentActivate() else intentList())
+            finish()
+        }
+
+        findViewById<Button>(R.id.btnList).setOnClickListener {
+            startActivity(intentList())
+            finish()
+        }
+
+        findViewById<Button>(R.id.btnLater).setOnClickListener {
+            finish()
         }
     }
-    */
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    private fun intentActivate(): Intent {
+        val intent = Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER)
+        intent.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
+            ComponentName(this, DolphinsService::class.java))
+        return intent
+    }
+
+    private fun intentList(): Intent {
+        return Intent(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER)
+    }
 }
