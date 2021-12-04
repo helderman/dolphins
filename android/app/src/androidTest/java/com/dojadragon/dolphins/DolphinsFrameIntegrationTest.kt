fdf918d9b48082729437be4dc23603921c0d4b5a
Ruud Helderman <r.helderman@hccnet.nl>
2021-12-04 16:28:32 +0100

    Supporting some older API levels

--- android/app/src/androidTest/java/com/dojadragon/dolphins/DolphinsFrameIntegrationTest.kt
+++ android/app/src/androidTest/java/com/dojadragon/dolphins/DolphinsFrameIntegrationTest.kt
@@ -40,14 +40,14 @@ class DolphinsFrameIntegrationTest {
 
         frame.draw(canvas)
 
-        assertLessThan("Bitmap difference", 40000, bitmapDifference(expected, bitmap))
+        assertMax("Bitmap difference", 60000, bitmapDifference(expected, bitmap))
     }
 
     @Ignore("For development purposes only")
     @Test
     // Run this test to (re)generate the baseline PNG file for one of the 'real' tests.
-    // Filter Logcat with "BaselinePNG" to find the BASE64-encoded data. Feed the data into:
-    // grep -oP '\S+$' | base64 -d > frameXXX.png
+    // Filter Logcat with "OutputPNG" to find the BASE64-encoded data. Feed the data into:
+    // grep -oP ': \K\S+' | base64 -d > frameXXX.png
     fun generateBaselinePNG() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
@@ -57,7 +57,7 @@ class DolphinsFrameIntegrationTest {
 
         frame.draw(canvas)
 
-        Log.d("BaselinePNG", base64png(bitmap))
+        base64png(bitmap).lines().forEach { Log.d("OutputPNG", it) }
     }
 
     private fun base64png(bitmap: Bitmap): String {
@@ -74,8 +74,9 @@ class DolphinsFrameIntegrationTest {
             BitmapFactory.Options().apply { inScaled = false }
         )
 
-    private fun assertLessThan(message: String, expected: Int, actual: Int) =
-        assertTrue("$message = $actual, expected < $expected", actual < expected)
+    // Asserts that the actual value is less than or equal to the expected value.
+    private fun assertMax(message: String, expected: Int, actual: Int) =
+        assertTrue("$message = $actual, expected <= $expected", actual <= expected)
 
     private fun bitmapDifference(bitmap1: Bitmap, bitmap2: Bitmap): Int =
         (toPixels(bitmap1) zip toPixels(bitmap2)).sumOf { colorDifference(it.first, it.second) }
