2c442b765522741eb653b8672167adae9de8b4bf
Ruud Helderman <r.helderman@hccnet.nl>
2021-12-28 01:12:37 +0100

    Using Choreographer

--- android/app/src/main/java/com/dojadragon/dolphins/DolphinsAnimationFactory.kt
+++ android/app/src/main/java/com/dojadragon/dolphins/DolphinsAnimationFactory.kt
@@ -1,13 +1,33 @@
 package com.dojadragon.dolphins
 
+import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.view.Choreographer
+import androidx.annotation.RequiresApi
 
 // Creates an object that drives the animation
 
 class DolphinsAnimationFactory {
     fun create(step: () -> Unit): IDolphinsAnimation {
-        return DolphinsAnimationByDelay(step)
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            DolphinsAnimationByVSync(step)
+        } else {
+            DolphinsAnimationByDelay(step)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
+    private class DolphinsAnimationByVSync(step: () -> Unit) : IDolphinsAnimation {
+        private val choreographer = Choreographer.getInstance()
+        private val callback = Choreographer.FrameCallback { step() }
+
+        override fun animate(visible: Boolean) {
+            choreographer.removeFrameCallback(callback)
+            if (visible) {
+                choreographer.postFrameCallback(callback)
+            }
+        }
     }
 
     private class DolphinsAnimationByDelay(step: () -> Unit) : IDolphinsAnimation {
