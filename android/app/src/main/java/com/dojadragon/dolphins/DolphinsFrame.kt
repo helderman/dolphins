c2aa6d959f5b12a0b13035e2a534485d766379d3
helderman <r.helderman@hccnet.nl>
2022-02-06 19:26:09 +0100

    Swallow IllegalArgumentException occurring in unlockCanvasAndPost

--- android/app/src/main/java/com/dojadragon/dolphins/DolphinsFrame.kt
+++ android/app/src/main/java/com/dojadragon/dolphins/DolphinsFrame.kt
@@ -1,24 +1,25 @@
 package com.dojadragon.dolphins
 
 import android.graphics.Canvas
+import android.util.Log
 import android.view.SurfaceHolder
 
 // Draw a single frame for the animation on canvas
 // Can be used both in a Live Wallpaper and in a regular SurfaceView
 
 class DolphinsFrame(private val painter: DolphinsPainter, private val canvasFactory: DolphinsCanvasFactory) {
-    fun drawFrame(surfaceHolder : SurfaceHolder) {
-        var canvas : Canvas? = null
+    fun drawFrame(surfaceHolder: SurfaceHolder) {
+        val canvas: Canvas = surfaceHolder.lockCanvas()
         try {
-            canvas = surfaceHolder.lockCanvas()
-            if (canvas != null) {
-                canvas.save()
-                canvasFactory.create(canvas) { painter.draw(it) }
-            }
+            canvas.save()
+            canvasFactory.create(canvas) { painter.draw(it) }
+            canvas.restore()
         } finally {
-            if (canvas != null) {
-                canvas.restore()
+            try {
                 surfaceHolder.unlockCanvasAndPost(canvas)
+            } catch (ex: IllegalArgumentException) {
+                // Ignore; seems impossible to 100% prevent this
+                Log.d("Dolphins", "unlockCanvasAndPost failed", ex)
             }
         }
     }
