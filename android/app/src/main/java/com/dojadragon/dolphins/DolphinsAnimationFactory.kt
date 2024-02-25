3f253912aef0f260caae407c96d40c5e08257b13
helderman <r.helderman@hccnet.nl>
2024-02-25 02:23:47 +0100

    MinSdk 19 as dictated by the latest uiautomator

--- android/app/src/main/java/com/dojadragon/dolphins/DolphinsAnimationFactory.kt
+++ android/app/src/main/java/com/dojadragon/dolphins/DolphinsAnimationFactory.kt
@@ -1,23 +1,14 @@
 package com.dojadragon.dolphins
 
-import android.os.Build
-import android.os.Handler
-import android.os.Looper
 import android.view.Choreographer
-import androidx.annotation.RequiresApi
 
 // Creates an object that drives the animation
 
 class DolphinsAnimationFactory {
     fun create(step: () -> Unit): IDolphinsAnimation {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            DolphinsAnimationByVSync(step)
-        } else {
-            DolphinsAnimationByDelay(step)
-        }
+        return DolphinsAnimationByVSync(step)
     }
 
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     private class DolphinsAnimationByVSync(step: () -> Unit) : IDolphinsAnimation {
         private val choreographer = Choreographer.getInstance()
         private val callback = Choreographer.FrameCallback { step() }
@@ -27,22 +18,6 @@ class DolphinsAnimationFactory {
             if (visible) {
                 choreographer.postFrameCallback(callback)
             }
-        }
-    }
-
-    private class DolphinsAnimationByDelay(step: () -> Unit) : IDolphinsAnimation {
-        private val handler = Handler(Looper.getMainLooper())
-        private val runnable = Runnable { step() }
-
-        override fun animate(visible: Boolean) {
-            handler.removeCallbacks(runnable)
-            if (visible) {
-                handler.postDelayed(runnable, DELAY_MS)
-            }
-        }
-
-        companion object {
-            private const val DELAY_MS = 20L
         }
     }
 }
--- android/app/src/main/java/com/dojadragon/dolphins/MainActivity.kt
+++ android/app/src/main/java/com/dojadragon/dolphins/MainActivity.kt
@@ -3,35 +3,21 @@ package com.dojadragon.dolphins
 import android.app.WallpaperManager
 import android.content.ComponentName
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(intentActivateWallpaperWithFallback)
+        startActivity(intentActivateWallpaper)
         finish()
     }
 
-    private val intentActivateWallpaperWithFallback: Intent
-        get() =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
-                intentActivateWallpaper
-            else
-                intentListWallpapers
-
     private val intentActivateWallpaper: Intent
-        @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
         get() =
             Intent(WallpaperManager.ACTION_CHANGE_LIVE_WALLPAPER).also {
                 it.putExtra(WallpaperManager.EXTRA_LIVE_WALLPAPER_COMPONENT,
                     ComponentName(this, DolphinsWallpaperService::class.java))
             }
-
-    private val intentListWallpapers: Intent
-        get() =
-            Intent(WallpaperManager.ACTION_LIVE_WALLPAPER_CHOOSER)
 }
